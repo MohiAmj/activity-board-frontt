735ec7af4db7011362f98e5c842d1f276b2d2758
Nariman Jahanshahi <nariman.j.2008@gmail.com>
2025-07-25 22:04:53 +0330

    ADD: ngdoc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
+import { NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent } from "@ng-doc/app";
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true,
